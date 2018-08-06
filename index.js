5ba64d702ddc2e81c6bb27ad0847b2f4e5ee2e29
garycheng292@gmail.com <garycheng292@gmail.com>
2018-08-06 00:23:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Gary"
-var height = "71"
+var height = 71
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height)+1)
       
